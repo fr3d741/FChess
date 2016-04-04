d993688760f2b989e798d34f0193c3949a24e2df
Csaba Csernai <csaba_csernai@epam.com>
2016-04-04 09:34:10 +0200

    addded some initial functions

--- FChessRefactor/AI/data.cpp
+++ FChessRefactor/AI/data.cpp
@@ -1,2 +1,35 @@
 #include "data.h"
 
+int AiData::ValueOfState(AiData::Figure* state, int maxX, int maxY, Defs::EColors color)
+{
+    int sum = 0;
+    for (int x = 0; x < maxX * maxY; ++x)
+    {
+        int value = 0;
+        switch(state[x].type)
+        {
+            case Defs::King:
+                value = 12;
+                break;
+            case Defs::Queen:
+                value = 9;
+                break;
+            case Defs::Rook:
+                value = 6;
+                break;
+            case Defs::Knight:
+                value = 3;
+                break;
+            case Defs::Bishop:
+                value = 2;
+                break;
+            case Defs::Pawn:
+                value = 1;
+            default:
+                value = 0;
+        }
+        sum += value * (state[x].color & color?1:-1);
+    }
+
+return sum;
+}
--- FChessRefactor/AI/data.h
+++ FChessRefactor/AI/data.h
@@ -1,5 +1,39 @@
 #ifndef DATA_H
 #define DATA_H
 
+#ifdef _WIN32
+        #define AInt8 __int8
+#else
+        #define AInt8 int8_t;
+#endif
 
+#include <QList>
+
+#include "../Defines.h"
+
+namespace AiData {
+    union Figure{
+        AInt8 color : 2;
+        AInt8 type : 6;
+        AInt8 data;
+    };
+
+    struct Movement
+    {
+        AInt8 fromX :4;
+        AInt8 fromY :4;
+        AInt8 toX :4;
+        AInt8 toY :4;
+    };
+
+    struct StateNode
+    {
+        int value;
+        Movement move;
+        QList<StateNode> childrenNodes;
+    };
+
+    int ValueOfState(Figure *state, int maxX, int maxY, Defs::EColors color);
+
+}
 #endif // DATA_H
